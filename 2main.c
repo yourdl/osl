fef1a765a0774e54d5ab76f3cfa690a06cd583ac
os <106179721+yourosl@users.noreply.github.com>
2022-11-13 18:56:41 +0530

    Update 2main.c

--- 2main.c
+++ 2main.c
@@ -1,65 +1,70 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<unistd.h>
-
-void sort(int a[10]);
-
-int main(int argc,char *argv[])
-{
-int pid;
-int i=0,n=10,search;
-char *env[]={NULL};
-int a[11];
-
-char *newarg[]={NULL,"sort.txt", NULL};
-newarg[0]=argv[1];
-
-printf("Enter array elements : ");
-for(i=1;i<=10;i++)
-scanf(" %d",&a[i]);
-
-
-
-printf("Enter value to find : ");
-scanf("%d", &search);
-FILE *f;
-pid=fork();
-
-if(pid==0)
-{
-sleep(1);
-execve(argv[1],newarg,env);
+#include<stdlib.h>
+void quicksort(int number[25],int first,int last){
+   int i, j, pivot, temp;
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+      while(i<j){
+         while(number[i]<=number[pivot]&&i<last)
+         i++;
+         while(number[j]>number[pivot])
+         j--;
+         if(i<j){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+      
+      temp=number[pivot];
+      number[pivot]=number[j];
+      number[j]=temp;
+      quicksort(number,first,j-1);
+      quicksort(number,j+1,last);
+   }
+   
 }
-else
-{
+int main(){
+   pid_t pid;
+   int status;
+    
+   int i, count, number[25];
+   printf("How many elements are u going to enter?: ");
+   scanf("%d",&count);
+   printf("Enter %d elements: ", count);
+   for(i=0;i<count;i++)
+   scanf("%d",&number[i]);
 
-sort(a);
-f=fopen("sort.txt","w");
+   pid=fork();
+   printf("%2d",pid);
+   if(pid==-1)
+   {
+    printf("\nError");
+   }
+   else if(pid>0)
+   { 
+    printf("\nparent procss");
+    printf("\nchild procss Id: %d parent process Id:%d",getpid(),getppid());
+    quicksort(number,0,count-1);
+    pid=wait(&status);
+   }
+   else
+   {
+    printf("\nchild procss");
+    printf("\nchild procss Id: %d parent process Id:%d",getpid(),getppid());
+     quicksort(number,0,count-1);
+     printf("\nSorted array:");
+     for (i=0;i<count;i++)
+     {
+        printf("%d\t",number[i]);
+     }
+     return 0;
+     
+   }
+   
 
-fprintf(f," %d",search);
-for(i=1;i<=n;i++)
-{
-fprintf(f," %d",a[i]);
-}
-
-}
-fclose(f);
-
-}
-
-void sort(int a[10])
-{
-int n=10, i=0, j=0, temp;
-for(i=1; i<=n; i++)
-{
-for(j=1; j<n; j++)
-{
-if(a[i]<a[j])
-{
-temp=a[i];
-a[i]=a[j];
-a[j]=temp;
-}
-}
-}
 }
