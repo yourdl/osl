71f5f5745c3ac2843a711bd410c5be2f9363f237
os <106179721+yourosl@users.noreply.github.com>
2022-11-14 07:37:53 +0530

    Update 2main.c

--- 2main.c
+++ 2main.c
@@ -2,69 +2,65 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdlib.h>
-void quicksort(int number[25],int first,int last){
-   int i, j, pivot, temp;
-   if(first<last){
-      pivot=first;
-      i=first;
-      j=last;
-      while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
-         i++;
-         while(number[j]>number[pivot])
-         j--;
-         if(i<j){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
-      }
-      
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
-   }
-   
+		
+void sort(int arr[30],int n)
+{
+	int i,j,temp;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printf("\n Ascending Order \n");
+	for(i=0;i<n;i++)
+		printf("\t%d",arr[i]);
+	printf("\n\n\n");
 }
-int main(){
-   pid_t pid;
-   int status;
-    
-   int i, count, number[25];
-   printf("How many elements are u going to enter?: ");
-   scanf("%d",&count);
-   printf("Enter %d elements: ", count);
-   for(i=0;i<count;i++)
-   scanf("%d",&number[i]);
 
-   pid=fork();
-   printf("%2d",pid);
-   if(pid==-1)
-   {
-    printf("\nError");
-   }
-   else if(pid>0)
-   { 
-    printf("\nparent procss");
-    printf("\nchild procss Id: %d parent process Id:%d",getpid(),getppid());
-    quicksort(number,0,count-1);
-    pid=wait(&status);
-   }
-   else
-   {
-    printf("\nchild procss");
-    printf("\nchild procss Id: %d parent process Id:%d",getpid(),getppid());
-     quicksort(number,0,count-1);
-     printf("\nSorted array:");
-     for (i=0;i<count;i++)
-     {
-        printf("%d\t",number[i]);
-     }
-     return 0;
-     
-   }
-   
-
-}
+void forkeg()
+{
+ 	int arr[25],arr1[25],n,i,status;
+ 	printf("\nEnter the no of values in array");
+ 	scanf("%d",&n);
+ 	printf("\nEnter the array elements");
+ 	for(i=0;i<n;i++)
+    		scanf("%d",&arr[i]);
+ 	int pid=fork();
+  	if(pid==0)
+   	{
+      		sleep(10);
+      		printf("\nchild process\n");
+      		printf("child process id=%d\n",getpid());
+      		sort(arr,n);
+       		printf("\nElements Sorted Using bubble Sort");
+     		printf("\n");
+       		for(i=0;i<n;i++)
+    			printf("%d,",arr[i]);
+    		printf("\b");
+     		printf("\nparent process id=%d\n",getppid());
+     		//system("ps -x");
+       }    
+      else
+       {
+     		printf("\nparent process\n");
+     		printf("\nparent process id=%d\n",getppid());
+	 	sort(arr,n);	      
+		printf("Elements Sorted Using Bubble Sort");
+     		printf("\n");
+      		for(i=0;i<n;i++)
+    			printf("%d,",arr[i]);
+    		printf("\n\n\n"); 
+      } 
+ }     
+ int main()
+ {
+   	forkeg();
+   	return 0;
+ }
